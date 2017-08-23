c0e8eca26396c4c6d90606d1e6fda8afabf4de30
Young Ju Kim <pydemia@gmail.com>
2017-08-24 01:05:57 +0900

    Update data_handler.py

--- unipy/tools/data_handler.py
+++ unipy/tools/data_handler.py
@@ -27,6 +27,44 @@ __all__ = ['splitter',
 
 # A Function to split an Iterable into smaller chunks 
 def splitter(iterable, how='equal', size=2):
+    """
+    Summary
+    
+    This function splits an Iterable into the given size of multiple chunks.
+    The items of An iterable should be the same type.
+    
+    Parameters
+    ----------
+    iterable: Iterable
+        An Iterable to split.
+    
+    how: {'equal', 'remaining'}
+        The method to split.
+        'equal' is to split chunks with the approximate length
+        within the given size.
+        'remaining' is to split chunks with the given size,
+        and the remains are bound as the last chunk.
+    
+    size: int
+        The number of chunks.
+    
+    Returns
+    -------
+    list
+        A list of chunks.
+        
+    See Also
+    --------
+    
+    Examples
+    --------
+    >>> up.splitter(list(range(10)), how='equal', size=3)
+    [(0, 1, 2, 3), (4, 5, 6), (7, 8, 9)]
+    
+    >>> up.splitter(list(range(10)), how='remaining', size=3)
+    [(0, 1, 2), (3, 4, 5), (6, 7, 8), (9,)]
+    
+    """
     
     isinstance(iterable, collections.Iterable)
     isinstance(size, int)
@@ -39,7 +77,8 @@ def splitter(iterable, how='equal', size=2):
         
         if how == 'equal':
 
-            resList = np.array_split(iterable, (len(iterable) / size) + 1)
+            splitted = np.array_split(iterable, (len(iterable) / size) + 1)
+            resList = [tuple(chunks) for chunks in splitted]
             
             return resList
         
