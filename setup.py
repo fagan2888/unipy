4e359c00f61bd3d26b6663728e0f66fb289708ed
Young Ju Kim <pydemia@gmail.com>
2018-11-21 00:03:08 +0900

    no more python3.5 support 2nd

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 from distutils.cmd import Command
 
 
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 
 desc = """
 This contains a number of useful objects for data manipulation & analysis.
